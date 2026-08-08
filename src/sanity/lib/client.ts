6fbec26e61c5f251295322fb7ab3c687042bc3c5
sefvanp <sefvanp@gmail.com>
2026-08-08 22:00:07 +0530

    Update sanity client useCdn to false and add token

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -4,5 +4,6 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "",
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   apiVersion: "2024-01-01",
-  useCdn: true,
+  useCdn: false, 
+  token: process.env.SANITY_API_TOKEN,
 });
